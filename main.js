32e7b878757f08fa79c08e2be92a21ef7c65159a
oabuasba <omarabuasba8234@gmail.com>
2024-07-02 17:01:06 -0700

    fixed formatting & added projects

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-document.getElementById('progLangs').addEventListener('click', function() {
-    var hiddenContent = document.querySelector('.hiddenProgLangs');
-    if (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') {
-      hiddenContent.style.display = 'block';
-    } else {
-        hiddenContent.style.display = 'none';
-    }
-  });
+// document.getElementById('progLangs').addEventListener('click', function() {
+//     var hiddenContent = document.querySelector('.hiddenProgLangs');
+//     if (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') {
+//       hiddenContent.style.display = 'block';
+//     } else {
+//         hiddenContent.style.display = 'none';
+//     }
+// //   })
+
+document.getElementById('repoBtn1').addEventListener('click', function() {
+    window.open('https://github.com/bwally0/depop-scraper-api', '_blank');
+});
+
+document.getElementById('repoBtn2').addEventListener('click', function() {
+    window.open('https://github.com/oabuasba/personal-website', '_blank');
+});
