d7b0a81b7b9c5515c31843b1198dac7e39cc72ef
oabuasba <omarabuasba8234@gmail.com>
2024-07-04 23:28:28 -0700

    added descriptions and animations to skills

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-// document.getElementById('progLangs').addEventListener('click', function() {
-//     var hiddenContent = document.querySelector('.hiddenProgLangs');
-//     if (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') {
-//       hiddenContent.style.display = 'block';
-//     } else {
-//         hiddenContent.style.display = 'none';
-//     }
-// //   })
+$(document).ready(function() {
+    $('#repoBtn1').click(function() {
+        window.open('https://github.com/bwally0/depop-scraper-api', '_blank');
+    });
 
-document.getElementById('repoBtn1').addEventListener('click', function() {
-    window.open('https://github.com/bwally0/depop-scraper-api', '_blank');
-});
-
-document.getElementById('repoBtn2').addEventListener('click', function() {
-    window.open('https://github.com/oabuasba/personal-website', '_blank');
+    $('#repoBtn2').click(function() {
+        window.open('https://github.com/oabuasba/personal-website', '_blank');
+    });
 });
